d5b11d3b4ff38c8066a1722576ceae9b291a254e
Gustavotc <gustavotch@hotmail.com>
2025-01-23 21:47:31 -0300

    chore: setup react-native-elements ui lib

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,19 @@
 import { Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Button } from '@rneui/themed';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Button title='Teste' onPress={() => console.log('teste')} />
+      </View>
+    </SafeAreaProvider>
   );
 }
