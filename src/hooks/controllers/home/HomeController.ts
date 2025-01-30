0ee2ca5195734ed8dff2ca0d96ffeca5f697eae3
Gustavotc <gustavotch@hotmail.com>
2025-01-29 23:31:00 -0300

    feat(classes): paginated classes fetch

--- src/hooks/controllers/home/HomeController.ts
+++ src/hooks/controllers/home/HomeController.ts
@@ -1,7 +1,17 @@
-import { useState } from 'react';
+import { useSession } from '@/src/contexts/AuthContext';
+import { IClass } from '@/src/domain/entities/Classes';
+import makeFetchTeacherClasses from '@/src/factories/usecases/FetchTeacherClassesFactory';
+import { useEffect, useRef, useState } from 'react';
 
 export const useHomeController = () => {
   const [showNewClassModal, setShowNewClassModal] = useState(false);
+  const [classes, setClasses] = useState<IClass[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const { user } = useSession();
+
+  const currentPage = useRef(1);
+  const hasMoreClassesToFetch = useRef(false);
 
   const handleCreateClass = () => {
     setShowNewClassModal(true);
@@ -11,5 +21,41 @@ export const useHomeController = () => {
     setShowNewClassModal(false);
   };
 
-  return { showNewClassModal, handleCreateClass, handleCloseCreateClassModal };
+  const updateClasses = async () => {
+    if (!user) return;
+
+    try {
+      setLoading(true);
+      const response = await makeFetchTeacherClasses().execute({
+        page: 1,
+        teacherId: user?.id,
+        limit: 10,
+      });
+
+      hasMoreClassesToFetch.current = response.totalPages > currentPage.current;
+
+      if (currentPage.current === 1) {
+        setClasses(response.data);
+      } else {
+        setClasses((oldState) => [...oldState, ...response.data]);
+      }
+    } catch {
+      // TODO - Mostrar toast
+      console.log('Error ao listar turmas');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    updateClasses();
+  }, []);
+
+  return {
+    showNewClassModal,
+    classes,
+    loading,
+    handleCreateClass,
+    handleCloseCreateClassModal,
+  };
 };
